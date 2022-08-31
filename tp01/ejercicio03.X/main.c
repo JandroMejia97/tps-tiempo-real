94961b2714317a9de11de023581118f9d75ac20a
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@gmail.com>
2022-08-31 03:50:58 -0300

    Ejercicio 3

--- tp01/ejercicio03.X/main.c
+++ tp01/ejercicio03.X/main.c
@@ -2,12 +2,25 @@
  * File:   main.c
  * Author: Alejandro Mejía
  *
- * Created on 31 de agosto de 2022, 03:28 AM
+ * Created on 30 de agosto de 2022, 08:34 PM
  */
 
 
-#include <xc.h>
+#include <htc.h>
 
-void main(void) {
-    return;
+#define _XTAL_FREQ 1000000UL // 1MHz
+
+int main(void) {
+    // RB4 y RB5 como salidas
+    TRISB = 0b11001111;
+    // Enciendo los LEDs
+    RB4 = 1;
+    RB5 = 1;
+    // RA0 y RA1 como entradas
+    TRISA |= (1 << RA0) | (1 << RA1);
+
+    while(1) {
+    
+    }
+    return 0; 
 }
